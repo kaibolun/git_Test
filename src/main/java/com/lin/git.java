8e9709a5022eea0504e938f4901e4c31ead19000
kaibolun <1045461391@qq.com>
2022-06-18 09:42:52 +0800

    3 commit

--- src/main/java/com/lin/git.java
+++ src/main/java/com/lin/git.java
@@ -4,5 +4,7 @@ public class git {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+
     }
 }
